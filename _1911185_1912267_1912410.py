a9ab090068bbe76897561329a1e79c2aa1e1527d
DuyyUyenn <89907819+DuyyUyenn@users.noreply.github.com>
2021-10-28 17:18:44 +0700

    Update _1911185_1912267_1912410.py

--- _1911185_1912267_1912410.py
+++ _1911185_1912267_1912410.py
@@ -106,3 +106,84 @@ def static_evaluation(cur_state, move):
         if right_sum_o==0:
             move_value+=p
     return move_value
+
+
+
+def static_evaluation2(cur_state, move):
+    cur_board = cur_state.blocks[move.index_local_board] # Bảng hiện tại
+    x = move.x
+    y = move.y
+    p = move.value #player value
+    o = p* -1
+    res = 0
+    array_o = [0, 0, 0, 0, 0, 0, 0, 0]
+    array_p = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    if cur_state.game_result(cur_board) != 0:
+        
+        for i in range (3):
+            for j in range (3):
+                if cur_board[i][j] == o:
+                    array_o[i] += 1
+                    array_o[j + 3] += 1
+                    if i == j:
+                        array_o[6] += 1
+                    if i + j == 2:
+                        array_o[7] += 1
+
+                if cur_board[i][j] == p:
+                    array_p[i] += 1
+                    array_p[j + 3] += 1
+                    if i == j:
+                        array_p[6] += 1
+                    if i + j == 2:
+                        array_p[7] += 1
+
+        if array_o[x] == 1 and array_p[x] == 0:
+            res += 1
+        elif array_o[x] == 2:
+            res += 3
+        if array_o[y + 3] == 1 and array_p[y + 3] == 0:
+            res += 1
+        elif array_o[y + 3] == 2:
+            res += 3
+        if x == y:
+            if array_o[6] == 1 and array_p[6] == 0:
+                res += 1
+            elif array_o[6] == 2:
+                res += 3
+        if x + y == 2:
+            if array_o[7] == 1 and array_p[7] == 0:
+                res += 1
+            elif array_o[7] == 2:
+                res += 3
+
+        if array_o[x] == 0 and array_p[x] == 1:
+            res += 1
+        elif array_p[x] == 2:
+            res += 5
+        if array_o[y + 3] == 0 and array_p[y + 3] == 1:
+            res += 1
+        elif array_p[y + 3] == 2:
+            res += 5
+        if x == y:
+            if array_o[6] == 0 and array_p[6] == 1:
+                res += 1
+            elif array_p[6] == 2:
+                res += 5
+        if x + y == 2:
+            if array_o[7] == 0 and array_p[7] == 1:
+                res += 1
+            elif array_p[7] == 2:
+                res += 3
+
+        if 3 * x + y == 4:
+            res -= 3
+            # print(res)
+    else: 
+        if cur_state.game_result(cur_state.blocks[3 * x + y]) != 0:
+            res += 10
+
+    if p == 1:
+        return res
+    else: return -res
